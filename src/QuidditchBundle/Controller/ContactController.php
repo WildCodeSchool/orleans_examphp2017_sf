43eb8ff2e5b0b93756de7fa20a350c690f384822
leclercweb <lavega45@yahoo.fr>
2017-01-23 16:44:05 +0100

    site

---
+++ src/QuidditchBundle/Controller/ContactController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace QuidditchBundle\Controller;
+
+use QuidditchBundle\Entity\Contact;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Contact controller.
+ *
+ * @Route("contact")
+ */
+class ContactController extends Controller
+{
+    /**
+     * Lists all contact entities.
+     *
+     * @Route("/", name="contact_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contacts = $em->getRepository('QuidditchBundle:Contact')->findAll();
+
+        return $this->render('contact/index.html.twig', array(
+            'contacts' => $contacts,
+        ));
+    }
+
+    /**
+     * Creates a new contact entity.
+     *
+     * @Route("/new", name="contact_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createForm('QuidditchBundle\Form\ContactType', $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $request->getSession()
+                ->getFlashBag()
+                ->add('success', 'Le mail est envoyé !');
+
+            $message = \Swift_Message::newInstance();
+            $message->setSubject("Message provenant de votre site");
+            $message->setFrom('monsite@yahoo.fr');
+            $message->setTo('sylvain@wildcodeschool.fr');
+            $message->setBody(
+                $this->renderView(
+                    'QuidditchBundle:Default:reponse.html.twig',
+                    array('contact'=>$contact)
+
+                ),
+                'text/html'
+            );
+            $this->get('mailer')->send($message);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush($contact);
+
+        }
+
+        return $this->render('contact/new.html.twig', array(
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a contact entity.
+     *
+     * @Route("/{id}", name="contact_show")
+     * @Method("GET")
+     */
+    public function showAction(Contact $contact)
+    {
+        $deleteForm = $this->createDeleteForm($contact);
+
+        return $this->render('contact/show.html.twig', array(
+            'contact' => $contact,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing contact entity.
+     *
+     * @Route("/{id}/edit", name="contact_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Contact $contact)
+    {
+        $deleteForm = $this->createDeleteForm($contact);
+        $editForm = $this->createForm('QuidditchBundle\Form\ContactType', $contact);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('contact_edit', array('id' => $contact->getId()));
+        }
+
+        return $this->render('contact/edit.html.twig', array(
+            'contact' => $contact,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a contact entity.
+     *
+     * @Route("/{id}", name="contact_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Contact $contact)
+    {
+        $form = $this->createDeleteForm($contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($contact);
+            $em->flush($contact);
+        }
+
+        return $this->redirectToRoute('contact_index');
+    }
+
+    /**
+     * Creates a form to delete a contact entity.
+     *
+     * @param Contact $contact The contact entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Contact $contact)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('contact_delete', array('id' => $contact->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
--- src/QuidditchBundle/Controller/DefaultController.php
+++ src/QuidditchBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace QuidditchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -14,4 +15,38 @@ class DefaultController extends Controller
     {
         return $this->render('QuidditchBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/contactez-moi" )
+     */
+    public function formulaireAction()
+    {
+        return $this->render('QuidditchBundle:Default:formulaire.html.twig');
+    }
+
+    /**
+     * @Route("/recherche" )
+     */
+    public function rechercheAction(Request $request)
+    {
+        $form = $this->createForm('QuidditchBundle\Form\RechercheType');
+        $form->handleRequest($request);
+        $em = $this->getDoctrine()->getManager();
+        $players = $em->getRepository('QuidditchBundle:Players')->findAll();
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $players = $em->getRepository('QuidditchBundle:Players')->findBy(["nom"=>$data['Recherche']]);
+            //return $this->redirectToRoute('recherche');
+        }
+        return $this->render('QuidditchBundle:Default:recherche.html.twig', [
+            'form'=>$form->createView(),
+            'players'=>$players,
+
+        ]);
+
+    }
+
+
 }
