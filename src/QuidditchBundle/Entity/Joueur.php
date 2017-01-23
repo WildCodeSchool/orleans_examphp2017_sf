8e882384a09c9d2d8d739df548320808d2b7d58d
louri45 <l.richard45@gmail.com>
2017-01-23 14:14:04 +0100

    crud / entitiy

---
+++ src/QuidditchBundle/Entity/Joueur.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace QuidditchBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Joueur
+ *
+ * @ORM\Table(name="joueur")
+ * @ORM\Entity(repositoryClass="QuidditchBundle\Repository\JoueurRepository")
+ */
+class Joueur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="experience", type="string", length=255)
+     */
+    private $experience;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Joueur
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set experience
+     *
+     * @param string $experience
+     *
+     * @return Joueur
+     */
+    public function setExperience($experience)
+    {
+        $this->experience = $experience;
+
+        return $this;
+    }
+
+    /**
+     * Get experience
+     *
+     * @return string
+     */
+    public function getExperience()
+    {
+        return $this->experience;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Joueur
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
