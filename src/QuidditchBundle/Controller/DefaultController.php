a604c5cf51d536db8f033e4354731541eda75274
MkLHX <mickael.lehoux@gmail.com>
2017-01-23 18:07:18 +0100

    last commit

--- src/QuidditchBundle/Controller/DefaultController.php
+++ src/QuidditchBundle/Controller/DefaultController.php
@@ -30,11 +30,10 @@ class DefaultController extends Controller
      */
     public function playAction(Request $request)
     {
-        $team = new Team;
 
 //        $form = $this->createForm('QuidditchBundle\Form\PlayForm',$team);
 
-        $form = $this->createFormBuilder($team)
+        $form = $this->createFormBuilder()
             ->add('name', EntityType::class,array(
                 'class'=>'QuidditchBundle:Team',
                 'choice_label'=> 'name',
@@ -51,9 +50,40 @@ class DefaultController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $teams = $form->getData();
 
+            $team1 = $teams['name'][0];
+//            var_dump($team1);
+            $team2 = $teams['name'][1];
+//            var_dump($team2);
+            $players1 = $team1->getPlayers();
+            $players2 = $team2->getPlayers();
+
+            $team1Xp ='';
+            $team1Age ='';
+            $team1Fatigue ='';
+            $team2Xp ='';
+            $team2Age ='';
+            $team2Fatigue ='';
+            foreach ($players1 as $player) {
+                $team1Xp += $player->getXp();
+                $team1Age += $player->getAge();
+                $team1Fatigue += $player->getTiredness();
+            }
+//            var_dump($team1Xp);
+//            var_dump($team1Age);
+//            var_dump($team1Fatigue);
+            foreach ($players2 as $player) {
+                $team2Xp += $player->getXp();
+                $team2Age += $player->getAge();
+                $team2Fatigue += $player->getTiredness();
+            }
 
             return $this->render('QuidditchBundle::result.html.twig',array(
-                'teams'=>$teams,
+                'xp1'=>$team1Xp,
+                'age1'=>$team1Age,
+                'fatigue1'=>$team1Fatigue,
+                'xp2'=>$team2Xp,
+                'age2'=>$team2Age,
+                'fatigue2'=>$team2Fatigue,
             ));
         }
 
